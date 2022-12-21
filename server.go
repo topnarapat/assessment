14a76acead44bccd35e032782f701c78e49e7d4a
narapat <top.narapat@gmail.com>
2022-12-21 19:26:38 +0700

    add function check authorization

--- server.go
+++ server.go
@@ -14,12 +14,28 @@ func healthHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, "OK")
 }
 
+func checkAuthHeader(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		authHeader := c.Request().Header.Get("Authorization")
+		if authHeader == "" {
+			return echo.NewHTTPError(http.StatusUnauthorized, "Authorization header is required")
+		}
+		if authHeader != "November 10, 2009" {
+			return echo.NewHTTPError(http.StatusUnauthorized, "Authorization header is invalid")
+		}
+		return next(c)
+	}
+}
+
 func main() {
 	fmt.Println("Please use server.go for main file")
 
 	expense.InitDB()
 
 	e := echo.New()
+
+	e.Use(checkAuthHeader)
+
 	e.GET("/health", healthHandler)
 	e.POST("/expenses", expense.CreateExpenseHandler)
 
